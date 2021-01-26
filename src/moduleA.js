d7719322e0bf03cef6d3a153c4c241b100493c3b
Will Marttala <wmarttala123@gmail.com>
2021-01-26 14:18:31 -0600

    Initial commit

---
+++ src/moduleA.js
@@ -0,0 +1,20 @@
+console.log('Running Module A')
+
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+require('./moduleB')
+
+const moduleBImports = require('./moduleB')
+
+moduleBImports.internalFunction()
+
+const moduleBImportsAgain = require('./moduleB')
+
+console.log(moduleBImportsAgain.internalVariable)
+
+console.log('Finished Running Module A')
